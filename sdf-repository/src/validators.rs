81ba6cd9d61d69677f5670fb476a54ad0fb7f87f
Jan Romann <jan.romann@uni-bremen.de>
2026-03-24 23:42:15 +0100

    chore(sdf-repository): add license headers

--- sdf-repository/src/validators.rs
+++ sdf-repository/src/validators.rs
@@ -1,3 +1,11 @@
+// Copyright 2026 Jan Romann
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: MIT
+
 use actix_web::{Error, dev::ServiceRequest, error::ErrorUnauthorized, guard::GuardContext};
 use actix_web_httpauth::extractors::basic::BasicAuth;
 use log::{info, warn};
